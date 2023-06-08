6e33c1bd7efb44ce9ff873d090575ea3fb097a7b
hrj2233 <hrj2233@gmail.com>
2023-06-08 17:22:22 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => 'frontendhrj2233-project',
+  // 아래 주소들만 out폴더에 만들어줘! (getServerSideProps 있는 페이지는 제외)
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/boards': { page: '/boards' },
+    '/404': { page: '/404' },
+  }),
 };
 
 module.exports = nextConfig;
